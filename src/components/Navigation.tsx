9e6e067fa544ed963c82e50e2cf216a1b4892344
ohtoyoyo <annabeltsik@proton.me>
2025-11-07 16:04:00 +0200

    Update Navigation.tsx

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,6 +1,5 @@
 import { Button } from "@/components/ui/button";
 import { Menu, X } from "lucide-react";
-import Image from "next/image";
 import { useState } from "react";
 
 const Navigation = () => {
